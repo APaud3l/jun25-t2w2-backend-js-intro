6f5cfa6c9c10d157660e80f2f1fb14f95da5eb67
APaud3l <aamod.paudel@coderacademy.edu.au>
2025-11-15 10:01:11 +1100

    feat: GET routes for the express API

--- src/index.js
+++ src/index.js
@@ -9,6 +9,42 @@ const app = express();
 // Define the port number
 const port = 3000;
 
+app.get('/', (request, response) => {
+    response.json({
+        "message": "Hello World!"
+    });
+});
+
+app.get('/about', (request, response) => {
+    response.json({
+        "message": "This is an about page."
+    });
+});
+
+app.get('/hello/:name', (request, response) => {
+    const userName = request.params.name;
+    response.json({
+        "message": `Hello from ${userName}`
+    });
+});
+
+app.get('/profile', (request, response) => {
+    // Let's say the profile info comes from the database
+    const userProfile = {
+        name: "Alice",
+        age: 10,
+        favouritePokemon: 'Pikachu'
+    };
+
+    // Send the response to the front-end
+    response.json({
+        "message": "Details about you",
+        "data": userProfile
+    });
+});
+
+
+
 // Configure the server to listen to the specified port
 app.listen(port, () => {
     console.log("Example EXpress server started on port: ", port);
