ee8e1f2246cca23e99a42a5ba6d4cbbde1b69d0e
APaud3l <aamod.paudel@coderacademy.edu.au>
2025-11-15 12:10:18 +1100

    feat: Modified model to have unique property for pokemon name

--- models/pokemon.js
+++ models/pokemon.js
@@ -4,7 +4,8 @@ const mongoose = require('mongoose');
 const pokemonSchema = new mongoose.Schema({
     name: {
         type: String,
-        required: true
+        required: true,
+        unique: true
     },
     type: {
         type: [String],
--- scripts/seed.js
+++ scripts/seed.js
@@ -5,31 +5,27 @@ const { connectDB, disconnectDB } = require("../utils/database");
 const starterPack = [
     {name: 'Bulbasaur', type: 1234},
     {name: 'Charmander', type: 'fire'},
-    {name: 'Squirtle', type: 'water'}
+    {name: 'Squirtle', type: 'water'},
+    {
+        name: 'Pikachu',
+        type: 'Electric',
+        level: 7
+    }
 ];
 
 async function run(){
     // 1. Connect to the DB
     await connectDB();
 
-    // 2. Create a new value (Pokemon)
-    const pika = new Pokemon({
-        name: 'Pikachu',
-        type: 'Electric',
-        level: 7
-    });
-
-    // 3. Store it in the database
-    await pika.save();
     // const insertedPokes = await Pokemon.insertMany(starterPack);
-    
+
     // Ordered: false ensures that it attemps the insert operation on all instances and skips on error
     const insertedPokes = await Pokemon.insertMany(starterPack, {ordered: false});
     console.log("Bulk Inserted Pokemon:", insertedPokes);
-    console.log("Saved: ", pika);
 
     // 4. Disconnect from the DB
     await disconnectDB();
+    console.log("Seeding completed and DB disconnected.");
 }
 
 run();
